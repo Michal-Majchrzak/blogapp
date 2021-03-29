08cc92c4b0fbbdd7c7772c3857df6a35f7fae157
Michał <mmajchrzak1@gmail.com>
2021-03-29 20:41:53 +0200

    Add refactor of create and edit entries

--- blog/routes.py
+++ blog/routes.py
@@ -12,34 +12,42 @@ def index():
 
 @app.route('/new-post/', methods=['GET', 'POST'])
 def create_entry():
-    form = EntryForm()
-    errors = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            entry = Entry(
-                title=form.title.data,
-                body=form.body.data,
-                is_published=form.is_published.data
-            )
-            db.session.add(entry)
-            db.session.commit()
-
-            flash('Nowy post został dodany. Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
-        else:
-            errors = form.errors
-    return render_template('entry_form.html', form=form, errors=errors)
+    return create_or_edit_entry()
 
 
 @app.route('/edit-post/<int:entry_id>', methods=['GET', 'POST'])
 def edit_entry(entry_id: int):
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
-    form = EntryForm(obj=entry)
+    return create_or_edit_entry(entry_id=entry_id)
+
+
+def create_or_edit_entry(**kwargs):
+    # GET
+    entry_id = kwargs.pop('entry_id', None)
+    form = EntryForm()
+    entry = None
     errors = None
+    if entry_id:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+
+    # POST
     if request.method == 'POST':
         if form.validate_on_submit():
-            form.populate_obj(entry)
-            db.session.commit()
-            flash('Zmiany w poscie zostały zapisane. Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
+            if entry_id:
+                form.populate_obj(entry)
+                db.session.commit()
+                flash('Zmiany w poscie zostały zapisane. '
+                      'Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
+            else:
+                entry = Entry(
+                    title=form.title.data,
+                    body=form.body.data,
+                    is_published=form.is_published.data
+                )
+                db.session.add(entry)
+                db.session.commit()
+                flash('Nowy post został dodany. '
+                      'Aby wyświetlić go na stronie głównej pamiętaj aby zaznaczyć "Wpis opublikowany"')
         else:
             errors = form.errors
     return render_template('entry_form.html', form=form, errors=errors)
