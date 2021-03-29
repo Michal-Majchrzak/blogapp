e709459b1432e7167f54d49f42cd1b28bdde02cd
Michał <mmajchrzak1@gmail.com>
2021-03-29 13:15:20 +0200

    Update spaces in __init__

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from faker import Faker
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +15,6 @@ from blog import routes, models
 @app.shell_context_processor
 def make_shell_context():
     return {
-      "db": db,
-      "Entry": models.Entry
+        "db": db,
+        "Entry": models.Entry
     }
