71249850c4481abce365eb227bda98bdaeb8a6e1
Michał <mmajchrzak1@gmail.com>
2021-03-30 14:30:07 +0200

    Update remove faker import

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from faker import Faker
 
 app = Flask(__name__)
 app.config.from_object(Config)
